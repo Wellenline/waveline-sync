9217343d0e565dabbc80be666165ef8e4697a4fd
Mihkel Baranov <mihkel.baranov@gmail.com>
2020-05-10 08:43:58 +0300

    Fix linting errors

--- src/app.ts
+++ src/app.ts
@@ -3,135 +3,129 @@ import path from "path";
 import { Sync } from "./sync";
 
 export enum Settings {
-    CONNECTED = "connected",
-    SERVER = "server",
-    KEY = "key",
+	CONNECTED = "connected",
+	SERVER = "server",
+	KEY = "key",
 }
 
 export class App {
 
-    /** QWindow instance */
-    public window: QMainWindow = new QMainWindow();
+	public static get instance(): App {
+		return App._instance ? App._instance : new App();
+	}
 
-    /** QSystemTray instance */
-    public tray: QSystemTrayIcon = new QSystemTrayIcon();
+	public set connected(status: boolean) {
+		this._connected = status;
+		this.settings.setValue(Settings.CONNECTED, status);
+	}
 
-    /** App settings cache */
-    public settings = new QSettings("Wellenline", "waveline-sync");
+	public get connected() {
+		return this._connected;
+	}
 
-    /** App menu */
-    public appMenu = new QMenu();
+	private static _instance: App;
 
-    /** App Sub menu */
-    public appSubMenu = new QMenu();
+	/** QWindow instance */
+	public window: QMainWindow = new QMainWindow();
 
+	/** QSystemTray instance */
+	public tray: QSystemTrayIcon = new QSystemTrayIcon();
 
-    public _actions: { quit?: QAction;  sync?: QAction;  connect?: QAction; } = {};
+	/** App settings cache */
+	public settings = new QSettings("Wellenline", "waveline-sync");
 
+	/** App menu */
+	public appMenu = new QMenu();
 
-    private static _instance: App;
+	/** App Sub menu */
+	public appSubMenu = new QMenu();
 
-    public static get instance(): App {
-        return App._instance ? App._instance: new App();
-    }
+	public _actions: { quit?: QAction; sync?: QAction; connect?: QAction; } = {};
 
-    private _connected = false;
-
-    public set connected(status: boolean) {
-        this._connected = status;
-        this.settings.setValue(Settings.CONNECTED, status);
-    }
-
-    public get connected() {
-        return this._connected;
-    }
+	private _connected = false;
 
 	public run() {
-        console.log("App running");
+		console.log("App running");
 
-        // Set window title
-        this.window.setWindowTitle("Waveline Sync");
+		// Set window title
+		this.window.setWindowTitle("Waveline Sync");
 
-        // Keep app open even if window (dialog) is closed
-        QApplication.instance().setQuitOnLastWindowClosed(false); 
+		// Keep app open even if window (dialog) is closed
+		QApplication.instance().setQuitOnLastWindowClosed(false);
 
-        // Setup tray
+		// Setup tray
 		this.tray.setIcon(this._icon("logo.png"));
 		this.tray.setToolTip("Waveliny Sync");
 		this.tray.setContextMenu(this.appMenu);
-        this.tray.show();
-        
-        // Setup actions
+		this.tray.show();
 
-        this._syncAction();
-        this._quitAppAction();
+		// Setup actions
 
-        (global as any).win = this.window;
-		(global as any).systemTray = this.tray; 
-    }
+		this._syncAction();
+		this._quitAppAction();
 
-    /**
-     * Get icon from path
-     * @param name icon name
-     */
-    public _icon(name: string) {
+		(global as any).win = this.window;
+		(global as any).systemTray = this.tray;
+	}
+
+	/**
+	 * Get neq QIcon by file name
+	 * @param name asset name
+	 */
+	public _icon(name: string) {
 		return new QIcon(this._assetAbsolutePath(name));
 	}
 
-    /**
-     * Load asset from assets dir
-     * @param name path
-     */
+	/**
+	 * Load asset from assets dir
+	 * @param name asset name
+	 */
 	public _assetAbsolutePath(name: string) {
 		return path.resolve(__dirname, `../assets/${name}`);
-    }
-    
-    // Menu Actions
+	}
 
-    private _syncAction() {
-        this._actions.sync = new QAction();
+	// Menu Actions
 
-        this._actions.sync.setText("Sync");
+	private _syncAction() {
+		this._actions.sync = new QAction();
 
-        this._addSubMenu("Add Directory", () => {
-            const dir = Sync.instance.onSelectDir();
+		this._actions.sync.setText("Sync");
 
-            if (dir) {
-                this._addSubMenu(dir, (action: QAction) => {
-                    this.appSubMenu.removeAction(action);
-                    // Stop watching for dir
-                    Sync.instance.unwatch([dir]);
-                });
-            }
-        });
+		this._addSubMenu("Add Directory", () => {
+			const dir = Sync.instance.onSelectDir();
 
-        // Attach submenu to sync action
-        this._actions.sync.setMenu(this.appSubMenu);
+			if (dir) {
+				this._addSubMenu(dir, (action: QAction) => {
+					this.appSubMenu.removeAction(action);
+					// Stop watching for dir
+					Sync.instance.unwatch([dir]);
+				});
+			}
+		});
 
+		// Attach submenu to sync action
+		this._actions.sync.setMenu(this.appSubMenu);
 
 		this.appMenu.addAction(this._actions.sync);
 		this.appMenu.addSeparator();
-    }
-    /**
-     * Exit app action
-     */
-    private _quitAppAction() {
-        this._actions.quit = new QAction();
-        this._actions.quit.setText("Quit");
-        this._actions.quit.setShortcut(new QKeySequence("Alt+Q"));
-        this._actions.quit.addEventListener("triggered", () =>  QApplication.instance().exit(0));
-        this.appMenu.addAction( this._actions.quit);
-    }
+	}
 
+	/** Quit app action */
+	private _quitAppAction() {
+		this._actions.quit = new QAction();
+		this._actions.quit.setText("Quit");
+		this._actions.quit.setShortcut(new QKeySequence("Alt+Q"));
+		this._actions.quit.addEventListener("triggered", () => QApplication.instance().exit(0));
+		this.appMenu.addAction(this._actions.quit);
+	}
 
-    private _addSubMenu(text: string, cb: any) {
+	private _addSubMenu(text: string, cb: any) {
 		const action = new QAction();
 		action.setText(text);
 		action.addEventListener("triggered", () => cb(action));
 		this.appSubMenu.addAction(action);
 		this.appSubMenu.addSeparator();
 	}
-
 
 }
 
--- src/sync.ts
+++ src/sync.ts
@@ -3,43 +3,42 @@ import { FSWatcher, watch } from "chokidar";
 import { QFileDialog, FileMode } from "@nodegui/nodegui";
 
 export class Sync {
-    private ext: string[] = [".mp3", ".flac", ".m4a"];
-    private _fsWatcher!: FSWatcher;
-
-    private _paths: string[] = [];
-
-    private static _instance: Sync;
 
 	public static get instance(): Sync {
-		return Sync._instance ? Sync._instance: new Sync();
-    }
-    
-    public onSelectDir() {
-        const fileDialog = new QFileDialog();
+		return Sync._instance ? Sync._instance : new Sync();
+	}
+
+	private static _instance: Sync;
+	private ext: string[] = [".mp3", ".flac", ".m4a"];
+	private _fsWatcher!: FSWatcher;
+
+	private _paths: string[] = [];
+
+	public onSelectDir() {
+		const fileDialog = new QFileDialog();
 		fileDialog.setFileMode(FileMode.Directory);
-        fileDialog.exec();
+		fileDialog.exec();
 
-        if (fileDialog.result()) {
-            const selectedFiles = fileDialog.selectedFiles();
+		if (fileDialog.result()) {
+			const selectedFiles = fileDialog.selectedFiles();
 
-            this.watch(selectedFiles);
+			this.watch(selectedFiles);
 
-            return selectedFiles[0];
-        }
-    }
+			return selectedFiles[0];
+		}
+	}
 
-    /** Watch paths */
-    public watch(paths: string[]) {
-        console.log("Start watching", paths);
+	/** Watch paths */
+	public watch(paths: string[]) {
+		console.log("Start watching", paths);
 
-        if (!this._fsWatcher) {
+		if (!this._fsWatcher) {
+			// todo
+		}
+	}
 
-        }
-    }
-
-    /** Unwatch paths */
-    public unwatch(paths: string[]) {
-        console.log("Stop watching", paths);
-
-    }
+	/** Unwatch paths */
+	public unwatch(paths: string[]) {
+		console.log("Stop watching", paths);
+	}
 }
